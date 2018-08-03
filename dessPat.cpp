1c2249eb711cd761c83bf2588e47f58fcd323a1d
mayank bansal <mayank.bansal@mtree.co.in>
2018-08-03 19:25:10 +0530

    first commit

---
+++ dessPat.cpp
@@ -0,0 +1,31 @@
+/*
+ * dessPat.cpp
+ *
+ *  Created on: 03-Aug-2018
+ *      Author: mtree
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "dessPatConfig.h"
+int main (int argc, char *argv[])
+{
+    if (argc < 2)
+      {
+      fprintf(stdout,"%s Version %d.%d\n",
+              argv[0],
+              dessPat_VERSION_MAJOR,
+              dessPat_VERSION_MINOR);
+      fprintf(stdout,"Usage: %s number\n",argv[0]);
+      return 1;
+      }
+  double inputValue = atof(argv[1]);
+  double outputValue = sqrt(inputValue);
+  fprintf(stdout,"The square root of %g is %g\n",
+          inputValue, outputValue);
+  return 0;
+}
+
+
+
