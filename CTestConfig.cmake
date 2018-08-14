aba870e800bec1d4c989dbd73cca173164303ff1
mayank bansal <bansalmayank63@gmail.com>
2018-08-14 16:30:06 +0530

    CTestConfig 'not working'

---
+++ CTestConfig.cmake
@@ -0,0 +1,7 @@
+set (CTEST_PROJECT_NAME "dessPat")
+set (CTEST_DROP_METHOD "https")
+set (CTEST_DROP_SITE "open.cdash.org")
+set (CTEST_DROP_LOCATION "/index.php?project=PublicDashboard")
+set (CTEST_DROP_SITE_CDASH TRUE)
+
+
