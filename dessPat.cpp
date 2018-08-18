07f66b1e51f3127d6d84c5e6abbae71c9ccce54e
mayank bansal <bansalmayank63@gmail.com>
2018-08-18 17:33:01 +0530

    cmake organizing

--- dessPat.cpp
+++ dessPat.cpp
@@ -20,24 +20,13 @@
 #endif
 int main (int argc, char *argv[])
 {
-    if (argc < 2)
-      {
       fprintf(stdout,"%s Version %d.%d\n",
               argv[0],
               dessPat_VERSION_MAJOR,
               dessPat_VERSION_MINOR);
-      fprintf(stdout,"Usage: %s number\n",argv[0]);
-      return 1;
-      }
-  double inputValue = atof(argv[1]);
 
 #ifdef GOF
-  double outputValue = mysqrt(inputValue);
-#else
-  double outputValue = sqrt(inputValue);
 #endif
-  fprintf(stdout,"The square root of %g is %g\n",
-          inputValue, outputValue);
 
 #ifdef GFG
   //get number of test cases
