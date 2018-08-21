5664481fce9735b09b0dbc2a8fc2ecdd57edba2e
mayank bansal <bansalmayank63@gmail.com>
2018-08-21 19:12:07 +0530

    doxygen docs

--- gfg/include/gfg.h
+++ gfg/include/gfg.h
@@ -1,3 +1,13 @@
+/**
+  \file gfg.h
+  \author mayank bansal
+
+  \brief this file contains solutions to some coding problems I am working on.
+
+  I am putting together this file to keep track of problems I have solved as
+  well as a reference when I need a quick brush up of common topics.
+  */
+
 #ifndef GFG_GFG_H_
 
 #ifdef __cplusplus
