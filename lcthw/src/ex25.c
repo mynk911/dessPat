ca0a98b571cc79e917ffad9272e5811b2d2fb964
mayank bansal <bansalmayank63@gmail.com>
2019-02-14 08:51:24 +0530

    lcthw ex25

---
+++ lcthw/src/ex25.c
@@ -0,0 +1,113 @@
+///
+/// \file ex25.c
+/// \brief ex 25 variable arguments
+/// \author Mayank Bansal
+///
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+#include "dbg.h"
+#include "lcthw.h"
+
+#define MAX_DATA 100
+
+int read_string(char** out_string, int max_buffer)
+{
+    *out_string = calloc(1, max_buffer - 1);
+    check_mem(*out_string);
+    char* result = fgets(*out_string, max_buffer, stdin);
+    check(result != NULL, "input error");
+    return 0;
+error:
+    if (*out_string)
+        free(*out_string);
+    *out_string = NULL;
+    return -1;
+}
+
+int read_int(int* out_int)
+{
+    char* input = NULL;
+    int rc = read_string(&input, MAX_DATA);
+    check(rc == 0, "failed to read number");
+    *out_int = atoi(input);
+    free(input);
+    return 0;
+error:
+    if (input)
+        free(input);
+    return -1;
+}
+
+int read_scan(const char* fmt, ...)
+{
+    int i = 0;
+    int rc = 0;
+    int* out_int = NULL;
+    char* out_char = NULL;
+    char **out_string = NULL;
+    int max_buffer = 0;
+
+    va_list argp;
+    va_start(argp, fmt);
+    for (i = 0; fmt[i] != '\0'; i++)
+    {
+        if (fmt[i] == '%')
+        {
+            i++;
+            switch (fmt[i])
+            {
+            case '\0':sentinel("Invalid format. you ended with %%");
+                break;
+            case 'd': out_int = va_arg(argp, int*);
+                rc = read_int(out_int);
+                check(rc == 0, "Failed to read int");
+                break;
+            case 'c': out_char = va_arg(argp, char*);
+                *out_char = fgetc(stdin);
+                break;
+            case's': max_buffer = va_arg(argp, int);
+                out_string = va_arg(argp, char**);
+                rc = read_string(out_string, max_buffer);
+                check(rc == 0, "failed to read string");
+                break;
+            default: sentinel("Invalid format");
+            }
+        }
+        else {
+            fgetc(stdin);
+        }
+        check(!feof(stdin) && !ferror(stdin), "Input error");
+    }
+    va_end(argp);
+    return 0;
+error:
+    va_end(argp);
+    return -1;
+}
+
+int ex25(int argc, char* argv[])
+{
+    char* first_name = NULL;
+    char initial = ' ';
+    char* last_name = NULL;
+    int age = 0;
+    
+    int rc = read_scan("%s", MAX_DATA, &first_name);
+    check(rc == 0, "failed fn");
+    rc = read_scan("%c\n", &initial);
+    check(rc == 0, "failed initial");
+    rc = read_scan("%s", MAX_DATA, &last_name);
+    check(rc == 0, "failed ln");
+    rc = read_scan("%d", &age);
+    check(rc == 0, "failed age");
+
+    printf("guy : %s %c %s %d", first_name, initial, last_name, age);
+    free(first_name);
+    free(last_name);
+    return 0;
+error:
+    return -1;
+}
